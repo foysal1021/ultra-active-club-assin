2cbfb18afb4fd4671e0d7eea4d8ca78fc748e50c
foysal1021 <foysala1021@gmail.com>
2022-09-30 21:48:47 +0600

    added blog component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 
 import './App.css';
+import Blog from './component/Blog/Blog';
 import PhysicallyActivity from './component/PhysicallyActivity/PhysicallyActivity';
 
 function App() {
   return (
     <div className="App">
       <PhysicallyActivity></PhysicallyActivity>
-      <footer className=' row p-3'>
-        <h2 className='text-center mb-5'> Blog Question </h2>
-         
-        <div className=' col-12  col-lg-4'>
-          <div className=' bg-light p-3 rounded my-sm2'>
-          <h4 className=' text-center'> How dose React Work?</h4>
-          <p className='py-3'>
-         <strong>ANS :</strong>  React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes
-          </p>
-          </div>
-        </div>
-
-        <div className=' col-12  col-lg-4'>
-        <div className=' bg-light p-3 rounded'>
-          <h4 className=' text-center'>props and state difference</h4>
-          <p className='py-3'>
-            <strong>State :</strong>
-            The state is an updatable structure that is used to contain data or information about the component and can change over time. The change in state can happen as a response to user action or system event. It is the heart of the react component which determines the behavior of the component and how it will render. A state must be kept as simple as possible. It represents the component's local state or information. It can only be accessed or modified inside the component or by the component directly.
-          </p>
-          <p>
-            <strong> Props:</strong>
-            Props are read-only components. It is an object which stores the value of attributes of a tag and work similar to the HTML attributes. It allows passing data from one component to other components. It is similar to function arguments and can be passed to the component the same way as arguments passed in a function. Props are immutable so we cannot modify the props from inside the component.
-          </p>
-          </div>
-        </div>
-
-        <div className=' col-12 col-lg-4'>
-        <div className=' bg-light p-3 rounded'>
-          <h4 className=' text-center'> What else is done without data load useing UseEffect</h4>
-          <p className='py-3'>
-            <ul>
-              <li> updating the DOM</li>
-              <li> setting up subscriptions </li>
-              <li> timers <strong>etc</strong></li>
-            </ul>
-          </p>
-          </div>
-        </div>
-
-      </footer>
+      <Blog></Blog>
     </div>
   );
 }
--- src/component/Break/Break.js
+++ src/component/Break/Break.js
@@ -22,11 +22,11 @@ const brakeTime = props.brakeTime;
         <div>
             <p> <strong className=' fs-5'>Add A Break </strong> </p>
             <div className='p-3 break-items d-flex justify-content-between bg-info rounded '>
-                <button  id='btn1' onClick={()=>brakeTime(document.getElementById('btn1').value)} value={10}> 10 </button>
-                <button  id='btn2' onClick={()=>brakeTime(document.getElementById('btn2').value)} value={20}> 20 </button>
-                <button  id='btn3' onClick={()=>brakeTime(document.getElementById('btn3').value)} value={30}> 30 </button>
-                <button  id='btn4' onClick={()=>brakeTime(document.getElementById('btn4').value)} value={40}> 40 </button>
-                <button  id='btn5' onClick={()=>brakeTime(document.getElementById('btn5').value)} value={50}> 50 </button>
+                <button  id='btn1' onClick={()=>brakeTime(document.getElementById('btn1').value)} value={10}> <strong>10<span>s</span></strong> </button>
+                <button  id='btn2' onClick={()=>brakeTime(document.getElementById('btn2').value)} value={20}> <strong>20<span>s</span></strong> </button>
+                <button  id='btn3' onClick={()=>brakeTime(document.getElementById('btn3').value)} value={30}> <strong>30<span>s</span></strong> </button>
+                <button  id='btn4' onClick={()=>brakeTime(document.getElementById('btn4').value)} value={40}> <strong>40<span>s</span></strong> </button>
+                <button  id='btn5' onClick={()=>brakeTime(document.getElementById('btn5').value)} value={50}> <strong>50<span>s</span></strong>  </button>
 
             </div>
         </div>
